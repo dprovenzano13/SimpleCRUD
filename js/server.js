88fab8a3d42d60a61b9dd8fb9c36623bd19847bd
David Provenzano <dprovenzano13@gmail.com>
2022-02-02 22:52:21 -0500

    Add form to index.html

--- js/server.js
+++ js/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.listen(3000, () => {
     console.log('listening on 3000')
 })
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
     res.sendFile('/Users/BeachCasino/Documents/Code/SimpleCRUD' + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
