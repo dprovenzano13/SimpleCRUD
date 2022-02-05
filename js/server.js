9635a855a51b6ef28deb849a2607672a845248c4
David Provenzano <dprovenzano13@gmail.com>
2022-02-05 15:31:52 -0500

    Add index.ejs file to views folder

--- js/server.js
+++ js/server.js
@@ -10,15 +10,19 @@ MongoClient.connect('mongodb+srv://yoda:starwars13@cluster0.aq26hzx.mongodb.net/
         console.log('Connected to Database')
         const db = client.db('star-wars-quotes')
         const quotesCollection = db.collection('quotes')
-        app.listen(3000, () => {
-            console.log('listening on 3000')
-        })
         
+        app.set('view engine', 'ejs')
+        
+
         app.use(bodyParser.urlencoded({ extended: true}))
         
         app.get('/', (req, res) => {
-            res.sendFile('/Users/BeachCasino/Documents/Code/SimpleCRUD' + '/index.html')
-        })
+            db.collection('quotes').find().toArray()
+              .then(results => {
+                console.log(results)
+              })
+              .catch(error => console.error(error))
+          })
         
         app.post('/quotes', (req, res) => {
             quotesCollection.insertOne(req.body)
@@ -26,6 +30,9 @@ MongoClient.connect('mongodb+srv://yoda:starwars13@cluster0.aq26hzx.mongodb.net/
                     res.redirect('/')
                 })
                 .catch(error => console.error(error))
+        })
+        app.listen(3000, () => {
+            console.log('listening on 3000')
         })
     })
     .catch(error => console.error(error))
