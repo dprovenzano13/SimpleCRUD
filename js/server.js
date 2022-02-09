f4c4581333dfbd7e6d9053ab8967698087da4784
David Provenzano <dprovenzano13@gmail.com>
2022-02-08 19:43:59 -0500

    Finish app with no styling

--- js/server.js
+++ js/server.js
@@ -15,6 +15,8 @@ MongoClient.connect('mongodb+srv://yoda:starwars13@cluster0.aq26hzx.mongodb.net/
 
 
         app.use(bodyParser.urlencoded({ extended: true}))
+        app.use(express.static('public'))
+        app.use(bodyParser.json())
         
         app.get('/', (req, res) => {
             db.collection('quotes').find().toArray()
@@ -31,6 +33,38 @@ MongoClient.connect('mongodb+srv://yoda:starwars13@cluster0.aq26hzx.mongodb.net/
                 })
                 .catch(error => console.error(error))
         })
+        app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+                {name: 'Yoda'},
+                {
+                    $set: {
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+                .then(result => {
+                    res.json('Success')
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne(
+                {name: req.body.name},
+            )
+            .then(result => {
+                if(result.deletedCount === 0){
+                    return res.json('No quote to delete')
+                }
+                res.json(`Deleted Darth Vadar's quote`)
+            })
+            .catch(error => console.error(error))
+        })
+
         app.listen(3000, () => {
             console.log('listening on 3000')
         })
